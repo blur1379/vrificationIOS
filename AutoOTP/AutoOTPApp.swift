e2513ffa4265c894ab098ebb68e5fe0084992afa
mohammadblor <blor1379@gmail.com>
2022-04-18 09:47:36 +0430

    Initial Commit

---
+++ AutoOTP/AutoOTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOTPApp.swift
+//  AutoOTP
+//
+//  Created by mohammad blor on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AutoOTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
